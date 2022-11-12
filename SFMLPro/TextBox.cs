dacfaa1e381af64b509414e9d7b87b6927990814
Jakub <92330684+thesigns@users.noreply.github.com>
2022-11-12 21:32:29 +0100

    Dodaj pliki projektów.

---
+++ SFMLPro/TextBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace SFMLPro {
+    public class TextBox {
+    }
+}
---
+++ SFMLProTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFMLPro;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+
+namespace SFMLProTest {
+    internal class Program {
+
+        private void main(string[] args) {
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+        }
+
+    }
+}
